e656e03c3e0eb74e9bc6d1a61878f265abe8d68a
Nikita <tolstik-nik@mail.ru>
2019-12-01 00:09:37 +0300

    Добавил случайную генерацию кол-ва фильмов

--- src/components/profile-rating.js
+++ src/components/profile-rating.js
@@ -1,4 +1,15 @@
-export const createProfileRatingTemplate = () => `<section class="header__profile profile">
-<p class="profile__rating">Movie Buff</p>
-<img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-</section>`;
+import {getRandomNumber} from '../util.js';
+import {ZERO} from '../const.js';
+
+const COUNT_MAX = 300;
+
+export const createProfileRatingTemplate = () => {
+
+  const countFilm = getRandomNumber(ZERO, COUNT_MAX);
+
+  return (
+    `<section class="header__profile profile">
+      <p class="profile__rating">${countFilm} Movie Buff</p>
+      <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+    </section>`);
+};
