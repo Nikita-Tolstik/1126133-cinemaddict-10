47237a759dce5c53342ddb1910e83238c3136572
Nikita <tolstik-nik@mail.ru>
2019-12-01 17:27:03 +0300

    Добавил в файлы дополнительные общие фукнкции и константы

--- src/util.js
+++ src/util.js
@@ -1,7 +1,30 @@
-import {ONE} from './const.js';
+import {ZERO, ONE, RANDOM_NUMBER} from './const.js';
+
+const MINUTE = 60;
 
 const getRandomNumber = (min, max) => {
   return min + Math.floor(Math.random() * (max + ONE - min));
 };
 
-export {getRandomNumber};
+const getRatingNumber = (min, max) => {
+  return getRandomNumber(min, max) + Number(Math.random().toFixed(1));
+};
+
+
+const getDescription = (allSentences, max) => {
+  return allSentences
+    .filter(() => Math.random() > RANDOM_NUMBER)
+    .slice(ZERO, getRandomNumber(ONE, max))
+    .join(` `);
+};
+
+const getTimeFilm = (time) => {
+
+  return time < MINUTE ? `${time}m` : `${Math.floor(time / MINUTE)}h ${time % MINUTE}m`;
+
+};
+
+
+export {getRandomNumber, getRatingNumber, getDescription, getTimeFilm};
+
+
