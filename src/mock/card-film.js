5f802610daa7e343af11a2a8cc6faf3909ae8de6
Nikita <tolstik-nik@mail.ru>
2019-12-01 17:34:43 +0300

    Доработал моки в карточке фильма

--- src/mock/card-film.js
+++ src/mock/card-film.js
@@ -1,96 +1,26 @@
-import {getRandomNumber} from '../util.js';
-import {ZERO, ONE, RANDOM_NUMBER} from '../const.js';
+import {getRandomNumber, getRatingNumber, getDescription, getTimeFilm} from '../util.js';
+import {ZERO, ONE, RANDOM_NUMBER, MAX_RATING, YEAR_MIN, YEAR_MAX, MINUTE_MIN, MINUTE_MAX, COMMENT_MAX, sentences, posters, films, genres} from '../const.js';
 
-
-const films = [
-  `Joker`,
-  `Avengers: Endgame`,
-  `Knives Out`,
-  `Ford v. Ferrari`,
-  `Doctor Sleep`,
-  `Zombieland: Double Tap`,
-  `Once Upon A Time In Hollywood`,
-  `Spider-Man: Far From Home`,
-  `John Wick: Chapter 3`,
-  `Shazam`,
-  `21 Bridges`,
-  `Harry Potter`,
-  `Green Book`,
-  `Mission: Impossible - Fallout`,
-  `Ready Player One`
-];
-
-const posters = [
-  `made-for-each-other.png`,
-  `popeye-meets-sinbad.png`,
-  `sagebrush-trail.jpg`,
-  `santa-claus-conquers-the-martians.jpg`,
-  `the-dance-of-life.jpg`,
-  `the-great-flamarion.jpg`,
-  `the-man-with-the-golden-arm.jpg`
-];
-
-const sentences = [
-  `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
-  `Cras aliquet varius magna, non porta ligula feugiat eget.`,
-  `Fusce tristique felis at fermentum pharetra.`,
-  `Aliquam id orci ut lectus varius viverra.`,
-  `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
-  `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
-  `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
-  `Sed sed nisi sed augue convallis suscipit in sed felis.`,
-  `Aliquam erat volutpat.`,
-  `Nunc fermentum tortor ac porta dapibus.`,
-  `In rutrum ac purus sit amet tempus.`
-];
-
-
-const genres = [
-  `Action`,
-  `Adventure`,
-  `Animation`,
-  `Biography`,
-  `Comedy`,
-  `Crime`,
-  `Drama`,
-  `Family`,
-  `Fantasy`,
-  `Western`,
-  `Thriller`
-];
-
-
-const MAX_RATING = 9;
 const SENTENCES_MAX = 3;
-const YEAR_MIN = 1920;
-const YEAR_MAX = 2020;
-const MINUTE_MIN = 60;
-const MINUTE_MAX = 140;
-const COMMENT_MAX = 200;
-
-
-const getRatingNumber = (min, max) => {
-  return getRandomNumber(min, max) + Number(Math.random().toFixed(1));
-};
-
-
-const getDescription = (allSentences) => {
-  return allSentences
-    .filter(() => Math.random() > RANDOM_NUMBER)
-    .slice(ZERO, getRandomNumber(ONE, SENTENCES_MAX))
-    .join(` `);
-};
-
 
 const generateFilmCard = () => ({
-  title: films[getRandomNumber(ZERO, films.length - ONE)],
-  image: posters[getRandomNumber(ZERO, posters.length - ONE)],
-  description: getDescription(sentences),
-  rating: getRatingNumber(ONE, MAX_RATING),
-  year: getRandomNumber(YEAR_MIN, YEAR_MAX),
-  time: getRandomNumber(MINUTE_MIN, MINUTE_MAX),
-  genre: genres[getRandomNumber(ZERO, genres.length)],
-  comment: getRandomNumber(ZERO, COMMENT_MAX)
+
+  filmInfo: {
+    title: films[getRandomNumber(ZERO, films.length - ONE)],
+    image: posters[getRandomNumber(ZERO, posters.length - ONE)],
+    description: getDescription(sentences, SENTENCES_MAX),
+    rating: getRatingNumber(ONE, MAX_RATING),
+    year: getRandomNumber(YEAR_MIN, YEAR_MAX),
+    time: getTimeFilm(getRandomNumber(MINUTE_MIN, MINUTE_MAX)),
+    genre: genres[getRandomNumber(ZERO, genres.length)],
+    comment: getRandomNumber(ZERO, COMMENT_MAX)
+  },
+
+  userDetails: {
+    isWathclist: Math.random() > RANDOM_NUMBER,
+    isHistory: Math.random() > RANDOM_NUMBER,
+    isFavorites: Math.random() > RANDOM_NUMBER
+  }
 
 });
 
