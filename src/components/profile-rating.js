6a57a7b119d78b586f58e1c039fb76d991a11ff6
Nikita <tolstik-nik@mail.ru>
2019-12-02 16:23:39 +0300

    Написал функцию для определения уровня пользователя

--- src/components/profile-rating.js
+++ src/components/profile-rating.js
@@ -1,15 +1,34 @@
 import {getRandomNumber} from '../util.js';
 import {ZERO} from '../const.js';
 
-const COUNT_MAX = 300;
+const COUNT_MAX = 30;
+
+const getRating = (number) => {
+  let level;
+  switch (true) {
+    case (number === 0):
+      level = ``;
+      break;
+    case (number >= 1 && number <= 10):
+      level = `Novice`;
+      break;
+    case (number >= 11 && number <= 20):
+      level = `Fan`;
+      break;
+    default:
+      level = `Movie Buff`;
+  }
+  return level;
+};
+
 
 export const createProfileRatingTemplate = () => {
 
-  const countFilm = getRandomNumber(ZERO, COUNT_MAX);
+  const userRating = getRating(getRandomNumber(ZERO, COUNT_MAX));
 
   return (
     `<section class="header__profile profile">
-      <p class="profile__rating">${countFilm} Movie Buff</p>
+      <p class="profile__rating">${userRating}</p>
       <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
     </section>`);
 };
