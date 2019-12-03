d4c588f243743658362596fb602241732e137dc8
Nikita <tolstik-nik@mail.ru>
2019-12-03 17:39:18 +0300

    Написал функцию по отрисовке extra блока фильмов

--- src/util.js
+++ src/util.js
@@ -1,6 +1,9 @@
 import {ZERO, ONE, RANDOM_NUMBER} from './const.js';
+import {createCardFilmTemplate} from './components/card-film.js';
+
 
 const MINUTE = 60;
+const TWO = 2;
 
 const getRandomNumber = (min, max) => {
   return min + Math.floor(Math.random() * (max + ONE - min));
@@ -24,7 +27,31 @@ const getTimeFilm = (time) => {
 
 };
 
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
 
-export {getRandomNumber, getRatingNumber, getDescription, getTimeFilm};
+// Отсортировка фильмов в блоки самые комментированные и рейтинговые
+const renderExtraFilmBlock = (cards, feature, blockElement, extraElement) => {
+
+  if (cards.length > ZERO) {
+
+    const sortCards = cards.slice().sort((a, b) => b.filmInfo[feature] - a.filmInfo[feature]);
+    const isSame = sortCards.every((card) => sortCards[ZERO].filmInfo[feature] === card.filmInfo[feature]);
+
+    if (isSame && sortCards[ZERO].filmInfo[feature] === ZERO) {
+      extraElement.remove();
+    } else if (isSame) {
+      new Array(TWO).fill(``).forEach(() => render(blockElement, createCardFilmTemplate(cards[getRandomNumber(ZERO, cards.length - ONE)]), `beforeend`));
+    } else {
+      sortCards.slice(ZERO, TWO).forEach((card) => render(blockElement, createCardFilmTemplate(card), `beforeend`));
+    }
+
+  } else {
+    extraElement.remove();
+  }
+};
+
+export {getRandomNumber, getRatingNumber, getDescription, getTimeFilm, render, renderExtraFilmBlock};
 
 
