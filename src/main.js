b1eab7182ad022183493c9d01ee10d534499b31b
Nikita <tolstik-nik@mail.ru>
2019-12-10 18:11:36 +0300

    Перенёс подключение компонента Sort в Controller

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import ProfileRatingComponent from './components/profile-rating.js';
 import SiteMenuComponent from './components/site-menu.js';
-import SortMenuComponent from './components/sort-menu.js';
 import PageController from './controller/page-controller.js';
 import {generateFilmCards} from './mock/card-film.js';
 import {render, RenderPosition} from './utils/render.js';
 
 const COUNT_MAIN_CARDS = 20;
-
 
 export const cards = generateFilmCards(COUNT_MAIN_CARDS);
 
@@ -14,9 +12,7 @@ const siteHeaderElement = document.querySelector(`.header`);
 render(siteHeaderElement, new ProfileRatingComponent(), RenderPosition.BEFOREEND);
 
 const siteMainElement = document.querySelector(`.main`);
-
 render(siteMainElement, new SiteMenuComponent(), RenderPosition.BEFOREEND);
-render(siteMainElement, new SortMenuComponent(), RenderPosition.BEFOREEND);
 
 const pageController = new PageController(siteMainElement);
 
