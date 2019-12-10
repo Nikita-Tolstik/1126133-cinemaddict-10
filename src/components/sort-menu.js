80a82162b4e8b219084457d5990b5d73994ba1b2
Nikita <tolstik-nik@mail.ru>
2019-12-10 18:12:59 +0300

    Добавил метод обработчика события в компонент Sort

--- src/components/sort-menu.js
+++ src/components/sort-menu.js
@@ -1,17 +1,54 @@
 import AbstractComponent from './abstract-component.js';
 
-const createSortMenuTemplate = () =>
+const SORT_TAG_NAME = `A`;
 
+export const SortType = {
+  DEFAULT: `default`,
+  DATE: `date`,
+  RATING: `rating`
+};
+
+const createSortMenuTemplate = () =>
   `<ul class="sort">
-    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" class="sort__button">Sort by date</a></li>
-    <li><a href="#" class="sort__button">Sort by rating</a></li>
-  </ul>`
-;
+     <li><a href="#" data-sort-type="${SortType.DEFAULT}" class="sort__button sort__button--active">Sort by default</a></li>
+     <li><a href="#" data-sort-type="${SortType.DATE}" class="sort__button">Sort by date</a></li>
+     <li><a href="#" data-sort-type="${SortType.RATING}" class="sort__button">Sort by rating</a></li>
+   </ul>`;
 
 export default class SortMenu extends AbstractComponent {
+  constructor() {
+    super();
+
+    this._currentSortType = SortType.DEFAULT;
+  }
 
   getTemplate() {
     return createSortMenuTemplate();
+  }
+
+  onSetSortTypeChange(handler) {
+    this.getElement().addEventListener(`click`, (evt) => {
+
+      evt.preventDefault();
+
+      if (evt.target.tagName !== SORT_TAG_NAME) {
+        return;
+      }
+
+      const sortType = evt.target.dataset.sortType;
+
+      if (this._currentSortType === sortType) {
+        return;
+      }
+
+      this.getElement().querySelector(`.sort__button--active`).classList.remove(`sort__button--active`);
+
+      evt.target.classList.add(`sort__button--active`);
+
+      this._currentSortType = sortType;
+
+      handler(this._currentSortType);
+
+    });
   }
 }
