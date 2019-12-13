5455a8ae2e05f12fad6b6092c5dc57bde4ee7f0e
Nikita <tolstik-nik@mail.ru>
2019-12-14 02:37:19 +0300

    Добавил функцию replace

--- src/utils/render.js
+++ src/utils/render.js
@@ -42,3 +42,16 @@ export const removePopup = (component) => {
   }
 
 };
+
+export const replace = (newComponent, oldComponent) => {
+  const parentElement = oldComponent.getElement().parentElement;
+  const newElement = newComponent.getElement();
+  const oldElement = oldComponent.getElement();
+
+  const isExistElements = !!(parentElement && newElement && oldElement);
+
+  if (isExistElements && parentElement.contains(oldElement)) {
+    parentElement.replaceChild(newElement, oldElement);
+  }
+
+};
