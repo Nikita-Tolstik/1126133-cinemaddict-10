f7dc1671c168e569925bc574088c9c86627d64ff
Nikita <tolstik-nik@mail.ru>
2019-12-21 20:03:01 +0300

    Подключил класс MovieModel в точку входа, рефакторинг кода

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import SiteMenuComponent from './components/site-menu.js';
 import PageController from './controller/page-controller.js';
 import {generateFilmCards} from './mock/card-film.js';
 import {render, RenderPosition} from './utils/render.js';
+import MoviesModel from './models/movies.js';
 
 const COUNT_MAIN_CARDS = 20;
 
@@ -14,7 +15,12 @@ render(siteHeaderElement, new ProfileRatingComponent(), RenderPosition.BEFOREEND
 const siteMainElement = document.querySelector(`.main`);
 render(siteMainElement, new SiteMenuComponent(), RenderPosition.BEFOREEND);
 
-const pageController = new PageController(siteMainElement);
 
-pageController.render(cards);
+const moviesModel = new MoviesModel();
+moviesModel.setMovies(cards);
+
+const pageController = new PageController(siteMainElement, moviesModel);
+
+pageController.render();
+
 
