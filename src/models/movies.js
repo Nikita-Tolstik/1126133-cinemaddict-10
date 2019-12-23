b4fd8580e77253ba334d8ca7167f862bd30360fa
Nikita <tolstik-nik@mail.ru>
2019-12-24 02:36:05 +0300

    Добавил возможность реагировать на изменения компонентов

--- src/models/movies.js
+++ src/models/movies.js
@@ -1,18 +1,34 @@
 import {ZERO, ONE} from '../const.js';
+import {FilterType} from '../const.js';
+import {getMoviesByFilter} from '../utils/filter.js';
 
 export default class Movies {
   constructor() {
     this._movies = [];
+    this._activeFilterType = FilterType.ALL;
 
     this._NO_ELEMENT = -1;
+
+    this._filterChangeHandlers = [];
+    this._dataChangeHandlers = [];
   }
 
   getMovies() {
+    return getMoviesByFilter(this._movies, this._activeFilterType);
+  }
+
+  getAllMovies() {
     return this._movies;
   }
 
   setMovies(movies) {
     this._movies = Array.from(movies);
+  }
+
+  // Активация фильтра
+  setFilter(filterType) {
+    this._activeFilterType = filterType;
+    this._filterChangeHandlers.forEach((handler) => handler());
   }
 
   updateMovies(id, movie) {
@@ -23,7 +39,17 @@ export default class Movies {
     }
 
     this._movies = [].concat(this._movies.slice(ZERO, index), movie, this._movies.slice(index + ONE));
+    this._dataChangeHandlers.forEach((handler) => handler());
 
     return true;
+  }
+
+  // установка обработчика изменения активного фильта
+  setOnFilterChange(handler) {
+    this._filterChangeHandlers.push(handler);
+  }
+
+  setOnDataChange(handler) {
+    this._dataChangeHandlers.push(handler);
   }
 }
