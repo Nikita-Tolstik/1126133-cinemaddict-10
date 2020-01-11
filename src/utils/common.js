21c3ec5abd7df8bff55d56b36c22ab58743a2040
Nikita <tolstik-nik@mail.ru>
2020-01-11 18:37:46 +0300

    Удалил данные для моков

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,4 +1,4 @@
-import {ZERO, ONE, RANDOM_NUMBER, SymbolName} from '../const.js';
+import {ZERO, ONE, SymbolName} from '../const.js';
 import moment from 'moment';
 
 const MINUTE = 60;
@@ -22,35 +22,11 @@ const FormatDate = {
   COMMENT: `YYYY/MM/DD HH:MM`
 };
 
-export const getRandomNumber = (min, max) => {
-  return min + Math.floor(Math.random() * (max + ONE - min));
-};
-
-export const getRatingNumber = (min, max) => {
-  return getRandomNumber(min, max) + Number(Math.random().toFixed(1));
-};
-
-export const getDescription = (allSentences, max) => {
-  return allSentences
-    .filter(() => Math.random() > RANDOM_NUMBER)
-    .slice(ZERO, getRandomNumber(ONE, max))
-    .join(` `);
-};
 
 export const getTimeFilm = (time) => {
 
   return time < MINUTE ? `${time + SymbolName.MINUTE}` : `${Math.floor(time / MINUTE) + SymbolName.HOUR} ${time % MINUTE + SymbolName.MINUTE}`;
 
-};
-
-export const getRandomDate = (number) => {
-
-  const targetDate = new Date();
-  const diffValue = getRandomNumber(ZERO, number);
-
-  targetDate.setTime(targetDate.getTime() - diffValue);
-
-  return targetDate.getTime();
 };
 
 export const formatReleaseDate = (date) => {
