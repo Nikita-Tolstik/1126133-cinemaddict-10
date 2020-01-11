3f60153842a4783da03819c2dbf73c4fc3ae9880
Nikita <tolstik-nik@mail.ru>
2020-01-11 18:03:13 +0300

    Получил с сервера комментарии и связал с карточкой фильма

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import {render, RenderPosition} from './utils/render.js';
 import MoviesModel from './models/movies.js';
 import {FilterType, TagName} from './const.js';
 
-const AUTHORIZATION = `Basic djds7395jsdls34ks3suf4f4s2d`;
+const AUTHORIZATION = `Basic djds7395jsdls34ks3suf4f4s2d=_=`;
 const END_POINT = `https://htmlacademy-es-10.appspot.com/cinemaddict`;
 
 const api = new API(END_POINT, AUTHORIZATION);
@@ -39,27 +39,24 @@ filterController.setOnScreenChange((activeFilter) => {
   }
 });
 
+
 api.getMovies()
   .then((movies) => {
-    console.log(1);
-
-    const hhh = movies.map((movie) => {
-
-      api.getComments(movie.filmInfo.id)
-        .then((response) => {
-          console.log(2);
-          movie.filmInfo.commentUsers = response;
-          return movie;
-        });
-      return movie;
-    });
-    return hhh;
-  })
-  .then((movies) => {
-    console.log(movies);
     moviesModel.setMovies(movies);
-    filterController.render();
-    pageController.render();
-  });
 
+    const commentsPromisses = movies.map((movie) => {
+
+      return api.getComments(movie.filmInfo.id)
+        .then((comments) => {
+          movie.filmInfo.commentUsers = comments;
+        });
+    });
+
+    Promise.all(commentsPromisses)
+      .then(() => {
+
+        filterController.render();
+        pageController.render();
+      });
+  });
 
